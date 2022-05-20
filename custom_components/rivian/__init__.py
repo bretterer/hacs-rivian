d0c0b9f35e626c1efa5bb56a01e1752617ac99d3
Brian Retterer <bretterer@gmail.com>
2022-05-19 20:58:53 -0400

    Update Manifest

--- custom_components/rivian/__init__.py
+++ custom_components/rivian/__init__.py
@@ -1,1 +1,13 @@
 """Rivian (Unofficial)"""
+
+import asyncio
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(
+    hass: HomeAssistant, config_entry: ConfigEntry
+):  # pylint: disable=unused-argument
+    """Disallow configuration via YAML."""
+    return True
